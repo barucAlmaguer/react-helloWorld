b91aced345bd1e9839a25875a3b253661eb867bc
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-04-07 01:58:09 -0500

    going bananas

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { AppRegistry, Image, View, Text } from 'react-native';
 
 export default class Bananas extends Component {
   render() {
@@ -7,7 +7,10 @@ export default class Bananas extends Component {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <Image source={pic} style={{ width: 193, height: 110 }} />
+      <View> 
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Text>This app just went bananas!!</Text>
+      </View>
     );
   }
 }
