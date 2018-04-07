95fd8f9d171119bb18452713a32938fac356f48b
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-04-07 11:51:51 -0500

    mastering flexybois

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class OneFlexyBoi extends Component {
   render() {
     return (
-      <View style={{flex:1}}>
-        <View style={{ flex:1, backgroundColor: 'powderblue' }} />
-        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-        <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{ height:20, backgroundColor: 'black'}}/>
+        <View style={{ height: 20, backgroundColor: 'red' }} />
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+          <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+          <View style={{ flex: 3, backgroundColor: 'steelblue' }}>
+            <Text style={{ margin: 20 }}>Text over blue sky</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+AppRegistry.registerComponent('AwesomeProject', () => OneFlexyBoi);
