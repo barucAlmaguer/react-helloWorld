30c3b2443656c3376bee74b8eea79c26b210e1f6
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-04-07 12:06:09 -0500

    pizza translator

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text } from 'react-native';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
 
-export default class OneFlexyBoi extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View style={{ height:20, backgroundColor: 'black'}}/>
-        <View style={{ height: 20, backgroundColor: 'red' }} />
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
-          <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-          <View style={{ flex: 3, backgroundColor: 'steelblue' }}>
-            <Text style={{color:'white'}}>Text over blue sky</Text>
-          </View>
-        </View>
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => OneFlexyBoi);
+AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
